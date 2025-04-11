cf0e64a9004e72e71144a2856c996be18ce6a969
Keegan DSouza <kedsouza@microsoft.com>
2025-04-11 14:12:33 -0500

    correct main.bicep file path

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ name=$USER-$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 5)
 
 az group create --name $name --location eastus
 
-az deployment group create --resource-group $name --template-file main.bicep
+az deployment group create --resource-group $name --template-file AppServiceLinuxQuickRepoAppService/main.bicep
