3521d15a90c38b45ff14870ff8cf23533ac3daf8
Keegan D'Souza <kedsouza@microsoft.com>
2025-04-11 15:52:18 -0500

    resynced path

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ name=$USER-$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 5)
 
 az group create --name $name --location eastus
 
-az deployment group create --resource-group $name --template-file AppServiceLinuxQuickRepo/main.bicep
+az deployment group create --resource-group $name --template-file main.bicep
