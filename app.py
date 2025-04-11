d43c942686dbab76f527780e717667d186890a0e
Keegan D'Souza <kedsouza@microsoft.com>
2025-04-11 17:15:12 -0500

    Added python colors

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import subprocess, os, json, random
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+print(bcolors.WARNING + "Warning: No active frommets remain. Continue?" + bcolors.ENDC)
+
+
 # Download and extract the necessary modules to run the script
-subprocess.run(["wget", "https://github.com/kedsouza/AppServiceLinuxQuickRepo/raw/refs/heads/main/modules.zip"])
-subprocess.run(["unzip", "modules.zip"])
-subprocess.run(["rm", "modules.zip"])
+subprocess.run(["wget", "-q", "https://github.com/kedsouza/AppServiceLinuxQuickRepo/raw/refs/heads/main/modules.zip"])
+subprocess.run(["unzip", "-q", "modules.zip"])
+subprocess.run(["rm","modules.zip"])
 
 
 bicep_code = {
@@ -14,15 +28,17 @@ bicep_code = {
 x = ''
 while x == '':
     try: 
-        x =  input ('Choose App Service Type: Blessed Image (1), Web App for Container Public Image (2), Web App for Container Azure Container Registry Private Image (3): \n')
+        print("Choose App Service Type:")
+        print()
+        print("Blessed Image: " + bcolors.WARNING + "(1)" + bcolors.ENDC)
+        print("Web App for Container Public Image: " + bcolors.WARNING + "(2)" + bcolors.ENDC)
+        print("Web App for Container Azure Container Registry Private Image: "  + bcolors.WARNING + "(3)" + bcolors.ENDC)
+        x = input("Enter 1,2, or 3: ")
         if int(x) not in ([1,2,3]):
             x = ''
             print('Incorrect value, enter: 1, 2, or 3')
     except ValueError:
         print('Incorrect Value, enter: 1, 2, or 3')
-
-
-
 
 if int(x) == 1:
     f = open ('main.bicep', 'w')
@@ -30,27 +46,31 @@ if int(x) == 1:
     f.write('\n')
     f.write(bicep_code['appserviceblessedimage'])
     f.close()
-
-    #subprocess.run(["bash", "run.sh"])
     deploy_name = subprocess.run(["az", "account", "show"], capture_output=True)
     name = json.loads(deploy_name.stdout)['user']['name']
     user_name = name.split('@')[0]
-    print(user_name)
     d_name = user_name + '-appserviceblessedimage-' + str(random.randint(0, 9))
-    #az group create --name $name --location eastus
-    subprocess.run(["az", "group", "create", "--name", d_name, "--location", "eastus"])
-    #az deployment group create --resource-group $name --template-file main.bicep
-    subprocess.run(["az", "deployment", "group", "create", "--resource-group", d_name, "--template-file", "main.bicep"])
+else if (int(x) == 2):
+    print('Not implemented')
+else if (int(x) == 3):
+    print('Not implemented')
+
+while done == False:
+    print("Select additional services to add: ")
+    print('Vnet Intergration ? ')
+    print('Private Endpoint')
+    print('Storage Mount Blob')
+    print('Storage Mount File')
+    print('App GateWay')
+    print('KeyVault')
 
 
-else:
-    print ('Not implemented yet:')
-# print('Add what feature you want to add')
-# print('Vnet Intergration ? ')
-# print('Private Endpoint')
-# print('Storage Mount: Blob / File ')
-# print('App GateWay')
-# print('Azure KeyVault')
 
+
+
+# #az group create --name $name --location eastus
+# subprocess.run(["az", "group", "create", "--name", d_name, "--location", "eastus"])
+# #az deployment group create --resource-group $name --template-file main.bicep
+# subprocess.run(["az", "deployment", "group", "create", "--resource-group", d_name, "--template-file", "main.bicep"])
 
 
