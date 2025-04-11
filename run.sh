45937c894d2ff1511f872f4144b4a930b8477ee5
Keegan D'Souza <kedsouza@microsoft.com>
2025-04-11 16:45:43 -0500

    Removed run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-name=$USER-$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 5)
-
-az group create --name $name --location eastus
-
-az deployment group create --resource-group $name --template-file main.bicep
